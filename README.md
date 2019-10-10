88baf84793e04ab2dbcf4649f2b4dab25515f0c5
prasa-dd-vp <prasad.venkatesh90@gmail.com>
2019-10-10 12:30:50 +0530

    Added files

---
+++ README.md
@@ -0,0 +1,1 @@
+Google Ngram API
---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+  name = 'google_ngram_api',         
+  packages = ['google_ngram_api'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'API to download google ngram data as csv file',   
+  author = 'Venkatesh Prasad',                   
+  author_email = 'prasad.venkatesh90@gmail.com',      
+  url = 'https://github.com/prasa-dd-vp/google_ngram_api',   
+  download_url = 'https://github.com/prasa-dd-vp/Google-Ngram-API/archive/v_01.tar.gz',    
+  keywords = ['google', 'googlengram', 'api', 'csvdownloader', 'ngramdata', 'googledata', 'csv', 'downlader'],   
+  install_requires=[            
+          'pandas',
+          'requests',
+          'gzip',
+          're',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
